4dc95f435ceaf1ce0981cc85717c57672b19fda5
Евгений Волохович <xJhetonx@yandex.ru>
2018-05-18 22:02:17 +0700

    Add simple stylesheet

--- PasswordKit/main.cpp
+++ PasswordKit/main.cpp
@@ -1,13 +1,32 @@
+#include <cassert>
+
 #include <QApplication>
+#include <QFile>
 
 #include "MainWindow.h"
 
+namespace {
+
+const QString STYLE_PATH = ":/resources/theme/style.qss";
+
+QString LoadStyle(const QString& file)
+{
+	QFile style(file);
+	assert(style.open(QFile::ReadOnly));
+	return style.readAll();
+}
+
+}
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication app(argc, argv);
+
+	Q_INIT_RESOURCE(PasswordKit);
+	app.setStyleSheet(app.styleSheet() + LoadStyle(STYLE_PATH));
+
 	MainWindow w;
 	w.show();
 
-	return a.exec();
+	return app.exec();
 }
