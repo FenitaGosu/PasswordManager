c65d255cdf432d56882da6f440ebf5ffed044828
Евгений Волохович <xJhetonx@yandex.ru>
2018-06-04 23:05:12 +0700

    The mechanism is transactional
    - added transaction manager interface
    
    #2

---
+++ DataBase/Interfaces/ITransactionManager.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+namespace DataBase {
+
+class IQuery;
+
+/**
+ * Transaction manager interface
+ */
+class ITransactionManager
+{
+public:
+	virtual ~ITransactionManager() = default;
+
+	virtual void Start() = 0;
+	virtual void Commit() = 0;
+	virtual void Rollback() = 0;
+	virtual std::shared_ptr<IQuery> GetQuery() const = 0;
+};
+
+}
