76cdb3d56ee599d1bd2d9b618cd0bf58622b9e27
Евгений Волохович <xJhetonx@yandex.ru>
2018-06-22 22:38:48 +0700

    Added dialog about

--- PasswordKit/Mediator/Mediator.cpp
+++ PasswordKit/Mediator/Mediator.cpp
@@ -5,6 +5,7 @@
 #include "Application/Application.h"
 
 #include "PasswordUI/Dialogs/PasswordGeneratorDialog.h"
+#include "PasswordUI/Dialogs/AboutDialog.h"
 
 #include "Mediator.h"
 
@@ -18,19 +19,19 @@ Mediator::Mediator(QObject* parent)
 
 void PasswordKit::Mediator::OnAboutClicked()
 {
-	QDialog aboutDialog(GetApp->GetMainWindow());
+	AboutDialog aboutDialog(GetApp->GetMainWindow());
 	aboutDialog.exec();
 }
 
 void Mediator::OnShowIndependentPasswordGeneratorDialog()
 {
-	PasswordGeneratorDialog d;
+	PasswordGeneratorDialog d(PasswordGeneratorDialog::Mode::Independent, GetApp->GetMainWindow());
 	d.exec();
 }
 
 void Mediator::OnShowEmbeddablePasswordGeneratorDialog(QString& pas)
 {
-	PasswordGeneratorDialog d(PasswordGeneratorDialog::Mode::Embeddable);
+	PasswordGeneratorDialog d(PasswordGeneratorDialog::Mode::Embeddable, GetApp->GetMainWindow());
 	if (d.exec())
 		pas = d.GetPassowrd();
 }
