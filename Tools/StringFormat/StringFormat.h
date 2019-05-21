20cd2d31009b6a8e8aba09099c85d13e8b10b9f0
Evgeniy Volohovich <evgeniyvolohovich@gmail.com>
2019-05-21 21:10:16 +0700

    Added std::forward to streamWrapper
    [TECH]

--- Tools/StringFormat/StringFormat.h
+++ Tools/StringFormat/StringFormat.h
@@ -10,10 +10,10 @@ public:
 	template<typename ... Args>
 	static std::string Format(const std::string& format, Args ... args)
 	{
-		const auto size = snprintf(nullptr, 0, format.c_str(), args ...) + 1;
+		const auto size = snprintf(nullptr, 0, format.c_str(), std::forward<Args>(args) ...) + 1;
 		auto* buf = new char[size];
 
-		snprintf(buf, size, format.c_str(), args ...);
+		snprintf(buf, size, format.c_str(), std::forward<Args>(args) ...);
 		const auto res = std::string(buf, buf + size - 1);
 
 		delete[] buf;
